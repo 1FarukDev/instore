ac8acbffb2b2a98a210ce80e0bdffbd6047cad80
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2023-04-07 18:41:00 +0100

    Started the category section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,39 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import HomeImage from "@/Public/assets/images/Image.png";
+import Chair from '@/Public/assets/images/chair.png'
 const ProductPage = () => {
   return (
     <>
       <Header />
       <PageImage />
-      {/* <Category /> */}
+      <Category />
       {/* <Data /> */}
     </>
   );
 };
 
 export default ProductPage;
+
+const Header = () => {
+  return (
+    <header className="flex m-auto my-4">
+      <div className="flex justify-center font-bold text-5xl lg:text-center leading-lo w-full  ">
+        Explore What <br /> Your Home Needs{" "}
+      </div>{" "}
+    </header>
+  );
+};
 const PageImage = () => {
   return (
-    <section className="relative">
+    <section className="relative h-96">
       <div className="absolute">
-      <Image src={HomeImage} alt="Chairs Image" />
+        <Image src={HomeImage} alt="Chairs Image" />
       </div>
       <div className="absolute left-3 top-32">
-        <p className="font-bold ">High quality sofa <br /> <span className="text-3xl">30% off</span></p>
+        <p className="font-bold ">
+          High quality sofa <br /> <span className="text-3xl">30% off</span>
+        </p>
       </div>
     </section>
   );
@@ -55,15 +68,18 @@ const PageImage = () => {
 //     </>
 //   );
 // };
-const Header = () => {
-  return (
-    <header className="flex m-auto my-4">
-      <div className="flex justify-center font-bold text-5xl lg:text-center leading-lo w-full  ">
-        Explore What <br /> Your Home Needs{" "}
-      </div>{" "}
-    </header>
-  );
-};
+
 const Category = () => {
-  return <h1>Helllo World</h1>;
+  return(
+    <section className="">
+       <h1 className="text-xl font-bold">Category</h1>
+       <div className="flex mt-2">
+          <div>
+            <div><Image src={Chair} alt='Chair Image'/></div>
+          </div>
+          <div> <div><Image src={Chair} alt='Chair Image'/></div></div>
+          <div> <div><Image src={Chair} alt='Chair Image'/></div></div>
+       </div>
+    </section>
+  );
 };
