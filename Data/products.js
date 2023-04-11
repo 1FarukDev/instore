2a5765d29eb1458bff165409c85f055d52626aac
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2023-04-11 17:10:19 +0100

    Not done anything

--- Data/products.js
+++ Data/products.js
@@ -4,60 +4,22 @@ export const products = [
     "title": "Unbranded Concrete Gloves",
     "price": 307,
     "description": "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive",
-    "images": [
-    "https://api.lorem.space/image/shoes?w=640&h=480&r=684",
-    "https://api.lorem.space/image/shoes?w=640&h=480&r=9696",
-    "https://api.lorem.space/image/shoes?w=640&h=480&r=9532"
-    ],
-    "creationAt": "2023-04-03T09:42:01.000Z",
-    "updatedAt": "2023-04-03T22:55:20.000Z",
-    "category": {
-    "id": 4,
-    "name": "Shoes",
-    "image": "https://api.lorem.space/image/shoes?w=640&h=480&r=1088",
-    "creationAt": "2023-04-03T09:42:01.000Z",
-    "updatedAt": "2023-04-03T09:42:01.000Z"
-    }
+    "images": ""
     },
     {
         "id": 14,
         "title": "Unbranded Concrete Gloves",
         "price": 307,
         "description": "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive",
-        "images": [
-        "https://api.lorem.space/image/shoes?w=640&h=480&r=684",
-        "https://api.lorem.space/image/shoes?w=640&h=480&r=9696",
-        "https://api.lorem.space/image/shoes?w=640&h=480&r=9532"
-        ],
-        "creationAt": "2023-04-03T09:42:01.000Z",
-        "updatedAt": "2023-04-03T22:55:20.000Z",
-        "category": {
-        "id": 4,
-        "name": "Shoes",
-        "image": "https://api.lorem.space/image/shoes?w=640&h=480&r=1088",
-        "creationAt": "2023-04-03T09:42:01.000Z",
-        "updatedAt": "2023-04-03T09:42:01.000Z"
-        }
+        "images": ''
+        
         },
         {
             "id": 14,
             "title": "Unbranded Concrete Gloves",
             "price": 307,
             "description": "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive",
-            "images": [
-            "https://api.lorem.space/image/shoes?w=640&h=480&r=684",
-            "https://api.lorem.space/image/shoes?w=640&h=480&r=9696",
-            "https://api.lorem.space/image/shoes?w=640&h=480&r=9532"
-            ],
-            "creationAt": "2023-04-03T09:42:01.000Z",
-            "updatedAt": "2023-04-03T22:55:20.000Z",
-            "category": {
-            "id": 4,
-            "name": "Shoes",
-            "image": "https://api.lorem.space/image/shoes?w=640&h=480&r=1088",
-            "creationAt": "2023-04-03T09:42:01.000Z",
-            "updatedAt": "2023-04-03T09:42:01.000Z"
-            }
+            "images": ''
             },
         
 ]
