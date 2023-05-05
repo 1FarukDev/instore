1e039b094c9c3fbcf2fe37374a34f851cd2a380c
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2023-05-05 12:28:22 +0100

    Successfully queried my data from sanity

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,107 +2,148 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import HomeImage from "@/Public/assets/images/Image.png";
 import categories from "@/Data/categories";
-const ProductPage = () => {
+const ProductPage = ({properties}) => {
+  console.log(properties);
   return (
     <>
-      <Header />
+      {/* <Header />
       <PageImage />
-        <CategoryList />
+      <CategoryList />
       <Data />
-      <Popular />
+      <Popular /> */}
+      <div>
+        Hello WOrld
+      </div>
+      {
+        properties.result.map(prop => {
+          return (
+                        <div key={prop.key} className="flex">
+                          <div className="p-2 relative text-center categ">
+                            <div className="">
+                            </div>
+                            <span className="absolute top-1/2 text-center items-center left-6">
+                              {prop.price}
+                            </span>
+                          </div>
+                        </div>
+                      );
+        })
+      }
     </>
   );
 };
 
 export default ProductPage;
 
-const Header = () => {
-  return (
-    <header className="flex m-auto my-4">
-      <div className="flex justify-center font-bold text-5xl lg:text-center leading-lo w-full  ">
-        Explore What <br /> Your Home Needs{" "}
-      </div>{" "}
-    </header>
-  );
-};
-const PageImage = () => {
-  return (
-    <section className="relative">
-      <div className="relative text-center w-full">
-        <div className="w-full">
-          <Image src={HomeImage} alt="Chair Image" />
-        </div>
-        <span className="absolute top-1/2 text-center items-center left-6">
-          {" "}
-          <p className="font-bold pl-2">
-            High quality sofa <br />{" "}
-            <span className="lg:text-3xl">30% off</span>
-          </p>
-        </span>
-      </div>
-    </section>
-  );
-};
-const CategoryList = () => {
-  return (
-    <section className="">
-      <h1 className="text-xl font-bold pl-2">Category</h1>
-      <div className="flex lg:overflow-hidden overflow-scroll">
-        {categories.map((category) => {
-          return (
-            <div key={category.key} className="flex">
-              <div className="p-2 relative text-center text-white categ">
-                <div className="">
-                  <Image
-                    src={`/assets/images/${category.Image}`}
-                    width={400}
-                    height={100}
-                    alt="Chair Image"
-                  />
-                </div>
-                <span className="absolute top-1/2 text-center items-center left-6">
-                  {category.name}
-                </span>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </section>
-  );
-};
+// const Header = () => {
+//   return (
+//     <header className="flex m-auto my-4">
+//       <div className="flex justify-center font-bold text-5xl lg:text-center leading-lo w-full  ">
+//         Explore What <br /> Your Home Needs{" "}
+//       </div>{" "}
+//     </header>
+//   );
+// };
+// const PageImage = () => {
+//   return (
+//     <section className="relative">
+//       <div className="relative text-center w-full">
+//         <div className="w-full">
+//           <Image src={HomeImage} alt="Chair Image" />
+//         </div>
+//         <span className="absolute top-1/2 text-center items-center left-6">
+//           {" "}
+//           <p className="font-bold pl-2">
+//             High quality sofa <br />{" "}
+//             <span className="lg:text-3xl">30% off</span>
+//           </p>
+//         </span>
+//       </div>
+//     </section>
+//   );
+// };
+// const CategoryList = () => {
+//   return (
+//     <section className="">
+//       <h1 className="text-xl font-bold pl-2">Category</h1>
+//       <div className="flex lg:overflow-hidden overflow-scroll">
+//         {categories.map((category) => {
+//           return (
+//             <div key={category.key} className="flex">
+//               <div className="p-2 relative text-center text-white categ">
+//                 <div className="">
+//                   <Image
+//                     src={`/assets/images/${category.Image}`}
+//                     width={400}
+//                     height={100}
+//                     alt="Chair Image"
+//                   />
+//                 </div>
+//                 <span className="absolute top-1/2 text-center items-center left-6">
+//                   {category.name}
+//                 </span>
+//               </div>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </section>
+//   );
+// };
 
-const Popular =() =>{
-  return(
-    <h1>Hello World</h1>
-  )
-}
-const Data = () => {
-  const [products, setProducts] = useState([]);
-  const fetchProducts = async () => {
-    const response = await fetch(`/api/products`);
-    const data = await response.json();
-    setProducts(data);
+// const Popular = ({ wears }) => {
+//   console.log(wears);
+//   return <h1>Hello World</h1>;
+// };
+// const Data = () => {
+//   const [products, setProducts] = useState([]);
+//   const fetchProducts = async () => {
+//     const response = await fetch(`/api/products`);
+//     const data = await response.json();
+//     setProducts(data);
+//   };
+//   useEffect(() => {
+//     fetchProducts();
+//   }, []);
+//   return (
+//     <>
+//       <div className="w-full">
+//         {products.map((product) => {
+//           if (product.category.name === "Watches") {
+//             return (
+//               <div key={product.id} className=" ">
+//                 {product.id} {product.title} | {product.category.name}
+//                 <Image src={product.images[1]} width={400} height={300}/>
+//               </div>
+//             );
+//           } else {
+//             return ``;
+//           }
+//         })}
+//       </div>
+//     </>
+//   );
+// };
+
+// export async function getStaticProps() {
+//   const response = await fetch(
+//     "https://e1esx4r6.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22post%22%5D%7B%0Aauthor%20-%3E%20%7Bname%7D%2C%0A%20%20price%0A%7D%0A"
+//   );
+//   const data = await response.json();
+//   // console.log(wea);
+//   return {
+//     props: {
+//       wears: data,
+//     },
+//   };
+// }
+export async function getStaticProps(context) {
+  const response = await fetch("https://e1esx4r6.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22post%22%5D%7B%0Aauthor%20-%3E%20%7Bname%7D%2C%0A%20%20price%0A%7D%0A");
+  const data = await response.json();
+
+  return {
+    props: {
+      properties: data,
+    },
   };
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-  return (
-    <>
-      <div className="w-full">
-        {products.map((product) => {
-          if (product.category.name === "Watches") {
-            return (
-              <div key={product.id} className=" ">
-                {product.id} {product.title} | {product.category.name}
-                <Image src={product.images[1]} width={400} height={300}/>
-              </div>
-            );
-          } else {
-            return ``;
-          }
-        })}
-      </div>
-    </>
-  );
-};
+}
