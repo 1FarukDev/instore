e3d6c81f2378682f906bba267f03030323e27c39
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2023-05-09 11:17:07 +0100

    80% done with the chair section

--- src/pages/chairs.js
+++ src/pages/chairs.js
@@ -22,14 +22,24 @@ const Chairs = ({ chair }) => {
         </div>
       </header>
 
-      <main className="flex">
+      <main className="flex flex-wrap m-auto">
         {chair.result.map((cha) => {
           return (
-            <div className="">
+            <div className=" flex flex-col m-auto">
+              <div className=" h-full my-6 bg-gray-100 rounded-2xl">
+                <div className="w-80 h-72">
+                  <Image
+                    src={cha.image}
+                    width={500}
+                    height={200}
+                    className="w-full h-full rounded-2xl"
+                  />
+                </div>
+              <div className="p-4">
+              <h2>{cha.title}</h2>
               <h2>{cha.price}</h2>
-              <h2>{cha.author.name}</h2>
-              <div className="w-1/3 h-96">
-                <Image src={cha.image} width={500} height={200} className="w-full h-full border-2 border-red-200" />
+              {/* <h2>{cha.author.name}</h2> */}
+              </div>
               </div>
             </div>
           );
@@ -46,7 +56,7 @@ export default Chairs;
 
 export async function getStaticProps(context) {
   const response = await fetch(
-    "https://e1esx4r6.api.sanity.io/v2021-10-21/data/query/production?query=%2F%2F%20*%5B_type%20%3D%3D%20%22post%22%5D%7B%0A%2F%2F%20%20%20price%2C%0A%2F%2F%20%20%20%20%20author%20-%3E%20%7Bname%7D%2C%0A%2F%2F%20%20%20%20%20key%2C%0A%2F%2F%20%20%20%20%22image%22%3A%20mainImage.asset-%3Eurl%2C%0A%2F%2F%20%20%20%20%20categories%0A%2F%2F%20%7D%0A%0A*%5B_type%20%3D%3D%22post%22%26%26%22Chair%22%20in%20categories%5B%5D-%3Etitle%5D%7B%0A%22categories%22%3Acategories%5B%5D-%3Etitle%2C%0A%20%20price%2C%0A%20%20%22image%22%3A%20mainImage.asset-%3Eurl%2C%0A%20%20author%20-%3E%20%7Bname%7D%2C%0A%20%20%0A%20%20%22image%22%3A%20mainImage.asset-%3Eurl%2C%0A%7D%0A%0A%20%20%0A%20%20"
+    "https://e1esx4r6.api.sanity.io/v2021-10-21/data/query/production?query=%2F%2F%20*%5B_type%20%3D%3D%20%22post%22%5D%7B%0A%2F%2F%20%20%20price%2C%0A%2F%2F%20%20%20%20%20author%20-%3E%20%7Bname%7D%2C%0A%2F%2F%20%20%20%20%20key%2C%0A%2F%2F%20%20%20%20%22image%22%3A%20mainImage.asset-%3Eurl%2C%0A%2F%2F%20%20%20%20%20categories%0A%2F%2F%20%7D%0A%0A*%5B_type%20%3D%3D%22post%22%26%26%22Chair%22%20in%20categories%5B%5D-%3Etitle%5D%7B%0A%22categories%22%3Acategories%5B%5D-%3Etitle%2C%0A%20%20title%2C%0A%20%20%22image%22%3A%20mainImage.asset-%3Eurl%2C%0A%20%20author%20-%3E%20%7Bname%7D%2C%0A%20%20price%2C%0A%20%20%0A%20%20%0A%20%20%22image%22%3A%20mainImage.asset-%3Eurl%2C%0A%7D%0A%0A%20%20%0A%20%20"
   );
   const data = await response.json();
   console.log(data);
